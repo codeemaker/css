c78a76e2f37173d06b92bb68a67e5453840e8a76
Gopinath Jayakumar <66761005+codeemaker@users.noreply.github.com>
2021-11-01 17:21:28 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
 var slider = document.getElementById("range");
 var val = document.getElementById("value");
-slider.oninput = function() {
-  var active = 250 * (this.value / 100);
-  val.style.width = active + 'px';
+var optbtn=document.getElementsByClassName("opt-btn")[0];
+var optlist=document.getElementsByClassName("opt-list")[0];
+var dataimg=document.getElementsByClassName("imgdt")[0];
+		var dots=document.getElementsByClassName("img-dots")[0].children;
+slider.oninput = function(e) {
+  var active = e.target.value;
+  val.style.width = active+"%";
 }
+optbtn.onclick=function(){
+	optlist.classList.toggle("nav-list");
+}
+var arr=["example.jpg","example2.jpg","example3.jpg"];
+	    var i=0;
+	    var ab=setInterval(()=>{
+	    for(var l=0;l<dots.length;l++){
+	    		if(l===i){
+	    		dots[l].style.backgroundColor="#cbdce0e9";
+	    		dots[l].style.padding="2px";
+	    		}
+	    		else{
+	    		dots[l].style.backgroundColor="blue";
+	    		dots[l].style.padding="0";
+	    		}
+	    	}
+	    if(i<arr.length){
+	    	dataimg.style.background="url('"+arr[i]+"') no-repeat";
+	    	dataimg.style.backgroundSize="cover";
+	    	i++;
+	    }
+	    else{
+	    	dataimg.style.background="url('"+arr[0]+"') no-repeat";
+	    	dataimg.style.backgroundSize="cover";
+	    	dots[0].style.backgroundColor="#cbdce0e9";
+	    	i=1;
+	    	}
+	    },2500);
